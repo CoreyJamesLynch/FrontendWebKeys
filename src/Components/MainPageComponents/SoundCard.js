e9ed9d273aa4d7f3122e97896e716cce3341cae3
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-15 19:55:21 -0400

    Prepared SoundCard validations

--- src/Components/MainPageComponents/SoundCard.js
+++ src/Components/MainPageComponents/SoundCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, Card, Button } from 'react-bootstrap';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 // Button should turn from green "primary" to red "danger" depending on if sound is active.
 const SoundCard = (props) => (
   <Container>
     <Card>
       <Card.Body>
-        <Card.Title>{props.sound.key}</Card.Title>
+        <Card.Title>{props.sound.letter}</Card.Title>
         <Card.Text>{props.sound.name}</Card.Text>
         <Button variant="primary">Play</Button>
       </Card.Body>
@@ -15,3 +17,10 @@ const SoundCard = (props) => (
 );
 
 export default SoundCard;
+
+SoundCard.propTypes = {
+  sound: propTypes.shape({
+    letter: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
+};
