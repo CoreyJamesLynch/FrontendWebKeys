3d6906be90f5dccf980050b48ec8248b05a6476f
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-16 13:06:15 -0400

    Setup LoginForm.js

--- src/Components/LoginPageComponents/LoginForm.js
+++ src/Components/LoginPageComponents/LoginForm.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-const Navbar = () => (
-  <div>
-    <h4>This will be a Navbar</h4>
-  </div>
+const LoginForm = () => (
+  <Form>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Username</Form.Label>
+      <Form.Control type="username" placeholder="Enter username" />
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+
+    <Button variant="primary" type="submit">
+      Login
+    </Button>
+  </Form>
 );
 
-export default Navbar;
+export default LoginForm;
