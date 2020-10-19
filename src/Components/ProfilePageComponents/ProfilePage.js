68c9e628b81f6e5c2afdf35a7470427cc954cf75
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-19 12:12:19 -0400

    Change ProfilePage.js to class component for lifecycle method componentDidMount(). Make API call for User.

--- src/Components/ProfilePageComponents/ProfilePage.js
+++ src/Components/ProfilePageComponents/ProfilePage.js
@@ -6,71 +6,82 @@ import Button from 'react-bootstrap/Button';
 
 import PlaylistCard from './PlaylistCard';
 
-const ProfilePage = () => {
-  // Make API call here and set equal to playlists
-  // Make API call here and set equal to user
-  const playlistCards = playlists.map((playlist) => (
-    <PlaylistCard key={playlist.id} playlist={playlist} />
-  ));
+const loggedInUser = 'http://localhost:3000/users/1/';
+const playlists = { id: 1, name: 'testPlaylist', user_id: 1 };
 
-  return (
-    <>
-      <Form>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridFirstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" placeholder={user.first_name} />
-          </Form.Group>
+class ProfilePage extends React.Component {
+  state = { stateUser: [] };
 
-          <Form.Group as={Col} controlId="formGridLastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" placeholder={user.last_name} />
-          </Form.Group>
-        </Form.Row>
+  componentDidMount() {
+    fetch(`${loggedInUser}`)
+      .then((resp) => resp.json())
+      .then((user) => this.setState({ stateUser: user }));
+  }
 
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridUsername">
-            <Form.Label>Current Username</Form.Label>
-            <Form.Control type="text" placeholder={user.username} />
-          </Form.Group>
+  render() {
+    const playlistCards = playlists.map((playlist) => (
+      <PlaylistCard key={playlist.id} playlist={playlist} />
+    ));
 
-          <Form.Group as={Col} controlId="formGridReUsername">
-            <Form.Label>New Username</Form.Label>
-            <Form.Control type="text" placeholder="" />
-          </Form.Group>
-        </Form.Row>
+    return (
+      <>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridFirstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" placeholder={user.first_name} />
+            </Form.Group>
 
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Current Email</Form.Label>
-            <Form.Control type="email" placeholder={user.email} />
-          </Form.Group>
+            <Form.Group as={Col} controlId="formGridLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" placeholder={user.last_name} />
+            </Form.Group>
+          </Form.Row>
 
-          <Form.Group as={Col} controlId="formGridReEmail">
-            <Form.Label>New Email</Form.Label>
-            <Form.Control type="email" placeholder="" />
-          </Form.Group>
-        </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridUsername">
+              <Form.Label>Current Username</Form.Label>
+              <Form.Control type="text" placeholder={user.username} />
+            </Form.Group>
 
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Current Password</Form.Label>
-            <Form.Control type="password" placeholder="" />
-          </Form.Group>
+            <Form.Group as={Col} controlId="formGridReUsername">
+              <Form.Label>New Username</Form.Label>
+              <Form.Control type="text" placeholder="" />
+            </Form.Group>
+          </Form.Row>
 
-          <Form.Group as={Col} controlId="formGridRePassword">
-            <Form.Label>New Password</Form.Label>
-            <Form.Control type="password" placeholder="" />
-          </Form.Group>
-        </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Current Email</Form.Label>
+              <Form.Control type="email" placeholder={user.email} />
+            </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Save
-        </Button>
-      </Form>
-      <Container>{playlistCards}</Container>
-    </>
-  );
-};
+            <Form.Group as={Col} controlId="formGridReEmail">
+              <Form.Label>New Email</Form.Label>
+              <Form.Control type="email" placeholder="" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Current Password</Form.Label>
+              <Form.Control type="password" placeholder="" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridRePassword">
+              <Form.Label>New Password</Form.Label>
+              <Form.Control type="password" placeholder="" />
+            </Form.Group>
+          </Form.Row>
+
+          <Button variant="primary" type="submit">
+            Save
+          </Button>
+        </Form>
+        <Container>{playlistCards}</Container>
+      </>
+    );
+  }
+}
 
 export default ProfilePage;
