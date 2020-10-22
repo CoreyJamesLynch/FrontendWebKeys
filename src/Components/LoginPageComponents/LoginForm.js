da49d9a8d668c43220e1ab264ac06a26ed835f4b
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-22 14:25:49 -0400

    Add forms to LoginPage.js

--- src/Components/LoginPageComponents/LoginForm.js
+++ src/Components/LoginPageComponents/LoginForm.js
@@ -2,22 +2,26 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const LoginForm = () => (
-  <Form>
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>Username</Form.Label>
-      <Form.Control type="username" placeholder="Enter username" />
-    </Form.Group>
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="username" placeholder="Enter username" />
+        </Form.Group>
 
-    <Form.Group controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
 
-    <Button variant="primary" type="submit">
-      Login
-    </Button>
-  </Form>
-);
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    );
+  }
+}
 
 export default LoginForm;
